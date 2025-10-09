489f7891650062733881d1909b5a1abebc5bc7fc
Explore Guys <guysexplore@gmail.com>
2025-10-09 23:36:49 +0530

    Revert "Added Extra d in word Reserve"
    This reverts commit 8c2f63823a408a7818826c414e28f84559d32ca6.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-center py-3'>
-            <p>ProShop &copy; {currentYear} - All Rights Reservedddddddddddd</p>
+            <p>ProShop &copy; {currentYear} - All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
